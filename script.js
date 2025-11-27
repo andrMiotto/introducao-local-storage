ffaea75b5a943bf414bad0fc2bacb38aa04b693b
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2025-11-27 19:33:43 -0300

    feat:commit

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    const botao = document.getElementById('save-button');
-    const nameInput = document.getElementById('name-input');
-    const storageKey = 'userName';
-    const nomeDigitado = nameInput.value;
+const botao = document.getElementById('save-button');
+const bemVindoTexto = document.querySelector('.bem-vindo');
 
-    const nomeSalvo = localStorage.get(nomeDigitado);
+botao.addEventListener('click', () => {
 
-    const bemVindo = document.querySelector('bem-vindo');
+    const nome = document.getElementById('name-input').value.trim();
 
-    botao.addEventListener('click', () => {
-
-
-        if (nomeSalvo == "") {
-
-        } else {
-            localStorage.setItem(storageKey, nomeDigitado);
-            console.log("Olá:", nomeDigitado);
-
-        }
-    });
-    
+    localStorage.setItem("nomeUsuario", nome);
+    bemVindoTexto.textContent = "Bem vindo(a) " + nome;
 });
 
+const nomeSalvo = localStorage.getItem("nomeUsuario");
+
+if (nomeSalvo) {
+    bemVindoTexto.textContent = "Bem vindo(a) " + nomeSalvo;
+}
+
+
+
+
+
+//----------------------------------taskList----------------------------------//
+
+
+const form = document.querySelector("form");
+const containerTask = document.getElementById("tarefas");
+
+let tarefas = localStorage.getItem("tarefas")
+    ? JSON.parse(localStorage.getItem("tarefas"))
+    : [];
+
+const mostrarTarefas = () => {
+    containerTask.innerHTML = "";
+
+    tarefas.forEach(e => {
+        containerTask.innerHTML += `
+            <li id="${e.id}">
+                <input type="checkbox" class="check" ${e.check ? "checked" : ""} />
+                <span>${e.nome}</span>
+                <span class="material-symbols-outlined delete">delete</span>
+            </li>
+        `;
+    });
+};
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let valor = e.target[0].value.trim();
+
+    if (valor.length > 0) {
+        tarefas.push({
+            nome: valor,
+            id: Math.random(),
+            check: false
+        });
+
+        localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+        mostrarTarefas();
+
+        e.target[0].value = "";
+    }
+});
+
+mostrarTarefas(); 
